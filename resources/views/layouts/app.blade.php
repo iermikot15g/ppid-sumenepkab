6bba61d34c6eafcac07bc62862269b2e68f8eedd
iermikot15g <iermiko.t15g@gmail.com>
2026-06-05 11:40:20 +0700

    Update README.md (Final)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'PPID Kabupaten Sumenep')</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('styles')
 </head>
